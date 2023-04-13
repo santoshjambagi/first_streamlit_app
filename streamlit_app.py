6260c815574819d000a6f03f8f9f9e3ff6a11791
santoshjambagi <130680284+santoshjambagi@users.noreply.github.com>
2023-04-13 20:46:57 +0530

    Update streamlit_app.py
    Added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('1. Poha')
+streamlit.text('2. Oats')
+streamlit.text('3. Sandwich')
+streamlit.text('4. Boiled eggs')
